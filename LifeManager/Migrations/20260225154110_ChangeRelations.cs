73e74c1512ec3835b6d6f9446e87d3efa8f4f362
Neyo03 <wagnerneo03@gmail.com>
2026-02-26 09:59:31 +0100

    migrations

---
+++ LifeManager/Migrations/20260225154110_ChangeRelations.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LifeManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_HomeId",
+                table: "Tags",
+                column: "HomeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_Homes_HomeId",
+                table: "Tags",
+                column: "HomeId",
+                principalTable: "Homes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_Homes_HomeId",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_HomeId",
+                table: "Tags");
+        }
+    }
+}
--- LifeManager/obj/Debug/net10.0/LifeManager.AssemblyInfo.cs
+++ LifeManager/obj/Debug/net10.0/LifeManager.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LifeManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c0b4690e70484ceb6411a20e42353398816d23e")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+568c7ba3786d3e6811201375668b3d7580a0c1a9")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("LifeManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LifeManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
