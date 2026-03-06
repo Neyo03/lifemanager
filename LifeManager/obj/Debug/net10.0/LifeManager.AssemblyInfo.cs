612fb71643bc4fca4bd96138855de5d05038375a
Neyo03 <wagnerneo03@gmail.com>
2026-03-06 17:28:42 +0100

    add responsive design and refresh ui ux

--- LifeManager/obj/Debug/net10.0/LifeManager.AssemblyInfo.cs
+++ LifeManager/obj/Debug/net10.0/LifeManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LifeManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff2b3e8ee153083bdd680272a0cd5eb3db9ab5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35213bbcdd0bccfaf47f246a47eaa833bb00ff20")]
 [assembly: System.Reflection.AssemblyProductAttribute("LifeManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LifeManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
